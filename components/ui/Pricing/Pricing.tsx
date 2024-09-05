4e83b1511e3fb8d1be9f2787093ed6a761ae1c3e
ksk385 <ksk385@gmail.com>
2024-09-05 13:23:20 -0400

    Stripe Webhook Calls Next, Local DB shows Products, Pricing Page lists one_time prices

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -30,7 +30,7 @@ interface Props {
   subscription: SubscriptionWithProduct | null;
 }
 
-type BillingInterval = 'lifetime' | 'year' | 'month';
+type BillingInterval = 'once' | 'year' | 'month';
 
 export default function Pricing({ user, products, subscription }: Props) {
   const intervals = Array.from(
@@ -42,7 +42,7 @@ export default function Pricing({ user, products, subscription }: Props) {
   );
   const router = useRouter();
   const [billingInterval, setBillingInterval] =
-    useState<BillingInterval>('month');
+    useState<BillingInterval>('once');
   const [priceIdLoading, setPriceIdLoading] = useState<string>();
   const currentPath = usePathname();
 
@@ -190,6 +190,58 @@ export default function Pricing({ user, products, subscription }: Props) {
                       className="block w-full py-2 mt-8 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900"
                     >
                       {subscription ? 'Manage' : 'Subscribe'}
+                    </Button>
+                  </div>
+                </div>
+              );
+            })}
+            {products.map((product) => {
+              const price = product?.prices?.find(
+                (price) => price.type === 'one_time'
+              );
+              if (!price) return null;
+
+              const priceString = new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: price.currency!,
+                minimumFractionDigits: 0
+              }).format((price?.unit_amount || 0) / 100);
+              return (
+                <div
+                  key={product.id}
+                  className={cn(
+                    'flex flex-col rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900',
+                    {
+                      'border border-pink-500': subscription
+                        ? product.name === subscription?.prices?.products?.name
+                        : product.name === 'Freelancer'
+                    },
+                    'flex-1', // This makes the flex item grow to fill the space
+                    'basis-1/3', // Assuming you want each card to take up roughly a third of the container's width
+                    'max-w-xs' // Sets a maximum width to the cards to prevent them from getting too large
+                  )}
+                >
+                  <div className="p-6">
+                    <h2 className="text-2xl font-semibold leading-6 text-white">
+                      {product.name}
+                    </h2>
+                    <p className="mt-4 text-zinc-300">{product.description}</p>
+                    <p className="mt-8">
+                      <span className="text-5xl font-extrabold white">
+                        {priceString}
+                      </span>
+                      <span className="text-base font-medium text-zinc-100">
+                        /{billingInterval}
+                      </span>
+                    </p>
+                    <Button
+                      variant="slim"
+                      type="button"
+                      loading={priceIdLoading === price.id}
+                      onClick={() => handleStripeCheckout(price)}
+                      className="block w-full py-2 mt-8 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900"
+                    >
+                      {subscription ? 'Manage' : 'Buy'}
                     </Button>
                   </div>
                 </div>
--- sst.config.ts
+++ sst.config.ts
@@ -3,12 +3,25 @@
 export default $config({
   app(input) {
     return {
-      name: "nextjs-subscription-payments",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
+      name: 'nextjs-subscription-payments',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws'
     };
   },
   async run() {
-    new sst.aws.Nextjs("MyWeb");
-  },
+    new sst.aws.Nextjs('MyWeb', {
+      // link: [supabaseServiceRoleSecret, stripeKeySecret, stripeWebhookSecret],
+      environment: {
+        NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY:
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+          process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+        STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+        SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY
+      }
+    });
+  }
 });
